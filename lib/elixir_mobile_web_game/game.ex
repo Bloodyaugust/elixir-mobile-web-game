d6c63bd18f448e92b2a5ed22043d70997e96c9fa
Greyson Richey <greysonrichey@gmail.com>
2020-04-12 02:08:04 -0600

    Add "error handling" for start and next_round

--- lib/elixir_mobile_web_game/game.ex
+++ lib/elixir_mobile_web_game/game.ex
@@ -10,9 +10,13 @@ defmodule ElixirMobileWebGame.Game do
   def start(%__MODULE__{state: :pending} = game),
     do: %{game | state: :started, start_time: DateTime.utc_now()}
 
+  def start(game), do: game
+
   def next_round(%__MODULE__{state: :started, current_round: @max_rounds} = game),
     do: %{game | state: :finished}
 
   def next_round(%__MODULE__{state: :started} = game),
     do: %{game | current_round: game.current_round + 1}
+
+  def next_round(game), do: game
 end
